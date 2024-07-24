63af52b1ca9b8c1db99969bb2a1cccc8296a4871
wackytsui <allan.altavano@gmail.com>
2024-07-24 08:25:01 +0800

    api

---
+++ src/api/movie-api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const API_KEY = 'af468b47558a0483ee60828cb8f72c98';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+axios.defaults.params = {
+  api_key: API_KEY,
+};
+
+export const fetchTrendingMovies = async () => {
+  const response = await axios.get(`trending/movie/day`);
+  return response.data.results;
+};
+
+export const fetchMovieByQuery = async movieName => {
+  const response = await axios.get(`search/movie?query=${movieName}`);
+  return response.data.results;
+};
+
+export const fetchMovieDetails = async movieId => {
+  const response = await axios.get(`movie/${movieId}`);
+  return response.data;
+};
+
+export const fetchMovieCast = async movieId => {
+  const response = await axios.get(`movie/${movieId}/credits?`);
+  return response.data.cast;
+};
+
+export const fetchMovieReviews = async movieId => {
+  const response = await axios.get(`movie/${movieId}/reviews?`);
+  return response.data.results;
+};
